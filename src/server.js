160b49422514a23f4227a8029462da252b35fe92
Erick Barreto <erick.barreto@agilethought.com>
2020-09-21 10:01:24 -0500

    Handlebars (templates)

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 //Initializations
@@ -7,7 +8,13 @@ const app = express();
 // Settings 
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join (__dirname, '/views' ));
-
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 //Middlewares
 app.use(express.urlencoded({extended: false}));
@@ -19,7 +26,7 @@ app.use(express.urlencoded({extended: false}));
 
 //Routes 
 app.get('/',(req, res) => {
-    res.send('hello world');
+    res.render('index');
 });
 
 
